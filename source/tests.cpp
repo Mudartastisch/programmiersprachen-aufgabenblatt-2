cad1734bdf39ad7d08971538cde24840ce1e29a7
Kai Lorenz <kailorenz96@aol.com>
2017-05-03 18:18:03 +0200

    Added Tests for operator-

--- source/tests.cpp
+++ source/tests.cpp
@@ -6,10 +6,18 @@
 
 TEST_CASE()
 {
+	//Test Vektoren
+	//Werte sind im namen
+	//vec_prove.... sind die Ziehlvektoren die ich erwarte
 	Vec2 vec_zero_zero{};
+	Vec2 vec_zero_one{ 0.0,1.0 };
 	Vec2 vec_one_zero{ 1.0,0.0 };
 	Vec2 vec_one_one{ 1.0,1.0 };
+	Vec2 vec_five_two{ 5.0,2.0 };
+	Vec2 vec_seven_one{ 7.0,1.0 };
+	
 
+	//Operator+
 	Vec2 vec_prove_one_zero{};
 	vec_prove_one_zero.operator+=(vec_one_zero);
 	REQUIRE(vec_prove_one_zero.x == 1.0); //Test 0+1=1
@@ -25,6 +33,25 @@ TEST_CASE()
 	vec_prove_two_two.operator+=(vec_one_one);
 	REQUIRE(vec_prove_two_two.x == 2.0); //Test 0+1+1=2
 	REQUIRE(vec_prove_two_two.y == 2.0); //Test 0+1+1=2
+
+
+	//Operator-
+	Vec2 vec_prove_zero_zero{1.0,1.0};
+	vec_prove_zero_zero.operator-=(vec_one_one);
+	REQUIRE(vec_prove_zero_zero.x == 0.0); //Test 1-1=0
+	REQUIRE(vec_prove_zero_zero.y == 0.0); //Test 1-1=0
+
+	Vec2 vec_prove_nTwo_zero{ 5.0,2.0 };
+	vec_prove_nTwo_zero.operator-=(vec_seven_one);
+	vec_prove_nTwo_zero.operator-=(vec_zero_one);
+	REQUIRE(vec_prove_nTwo_zero.x == -2.0); //Test 5-7-0=-2
+	REQUIRE(vec_prove_nTwo_zero.y == 0.0); //Test 2-1-1=0
+
+	Vec2 vec_prove_one_one{ 5.0,2.0 };
+	vec_prove_one_one.operator-=(vec_five_two);
+	vec_prove_one_one.operator+=(vec_one_one);
+	REQUIRE(vec_prove_zero_zero.x == 1.0); //Test 5-5+1=1
+	REQUIRE(vec_prove_zero_zero.y == 1.0); //Test 2-2+1=1
 }
 int main(int argc, char *argv[])
 {
