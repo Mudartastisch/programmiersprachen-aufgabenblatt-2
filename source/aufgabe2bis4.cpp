9edff66b6be67d815ef0f6908de1386aa0180367
Kai Lorenz <kailorenz96@aol.com>
2017-05-20 13:54:39 +0200

    finished 3.3

--- source/aufgabe2bis4.cpp
+++ source/aufgabe2bis4.cpp
@@ -4,8 +4,11 @@
 # include < iostream > // std :: cout
 # include < iterator > // std :: ostream_iterator < >
 # include < algorithm > // std :: reverse , std :: generate
+# include <set>
+#include <time.h>
 int main(int argc, char *argv[])
 {
+	srand(time (NULL));
 	std::list<int> rndint;
 	for (int i = 0; i < 100; i++) {
 		//int random = rand()%(max-min+1)+min
@@ -18,5 +21,13 @@ int main(int argc, char *argv[])
 	std::copy(std::begin(rndint), std::end(rndint), std::back_inserter(rndvec));
 
 	std::cout << "Testing size of random int vec: " << rndvec.size() << '\n';
-
-}
+	
+	std::set<int> insidelist (rndint.begin(),rndint.end());
+	for (int i = 0; i <= rndint.size(); i++) {
+		if (insidelist.count(i)!=1) {
+			std::cout << i << " is not an element of insidelist.\n";
+		}
+	}
+	std::cout << "There are " << insidelist.size() << " elementsnin the set";
+	
+} 
